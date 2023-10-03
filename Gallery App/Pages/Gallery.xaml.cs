45e58e2a8e3d45c43f855643c6b3a3b34c8c0f19
Undying-Merch <jonasnikolailj@gmail.com>
2023-10-03 11:15:18 +0200

    Gallery (Show Pictures)
    Made the functions to show the pictures when the page is loaded, and readied some functions for showing only specefic category, awaiting implemantation.

--- Gallery App/Pages/Gallery.xaml.cs
+++ Gallery App/Pages/Gallery.xaml.cs
@@ -8,14 +8,66 @@ public partial class Gallery : ContentPage
     List<Kategori> categories = new List<Kategori>();
     List<string> categoryNames = new List<string>();
     List<Gallery_Class> billeder = new List<Gallery_Class>();
-    int catId = 0;
+    List<Gallery_Class> bListe = new List<Gallery_Class>();
+
+    int catId = -1;
+    int pageCounter = 0;
+    private string url = "http://10.130.54.78:8000";
+
+    public void setImageList()
+    {
+        bListe.Clear();
+        if (catId == -1)
+        {
+            for (int i = 0; i < billeder.Count; i++)
+            {
+                bListe.Add(billeder[i]);
+            }
+        }
+        else
+        {
+            for(int i = 0;i < billeder.Count; i++)
+            {
+                if (billeder[i].kategori_id == catId)
+                {
+                    bListe.Add(billeder[i]);
+                }
+            }
+        }
+        setImage();
+    }
 
     public void setImage()
     {
-        if (catId == 0)
+        galleryImage0.Source = null;
+        galleryImage1.Source = null;
+        galleryImage2.Source = null;
+        galleryImage3.Source = null;
+        for (int i = 0 + pageCounter; i < 4 + pageCounter; i++)
         {
-            
+            var imageSource = ImageSource.FromUri(new Uri(url + bListe[i].billedet));
+            if (i > bListe.Count)
+            {
+                break;
+            }
+            if (i == 0 + pageCounter)
+            {
+                galleryImage0.Source = imageSource;
+            }
+            else if (i == 1 + pageCounter)
+            {
+                galleryImage1.Source = imageSource;
+            }
+            else if (i == 2 + pageCounter)
+            {
+                galleryImage2.Source = imageSource;
+            }
+            else if (i == 3 + pageCounter)
+            {
+                galleryImage3.Source = imageSource;
+            }
         }
+
     }
 
     public Gallery()
@@ -23,11 +75,14 @@ public partial class Gallery : ContentPage
 		InitializeComponent();
         billeder = conn.getAllBilleder();
         categories = conn.getCategories();
+        categoryNames.Add("Alle");
         for (int i = 0; i < categories.Count; i++)
         {
             categoryNames.Add(categories[i].navn);
         }
         categoryPicker.ItemsSource = categoryNames;
+        setImageList();
+        setImage();
     }
 
 
